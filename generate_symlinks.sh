20a35338686caa40a88ea7cc86405772be326f98
Rakhim Davletkaliyev <rakhim@Rakhims-MacBook-Pro.local>
2019-10-28 20:23:35 +0200

    update paths

--- generate_symlinks.sh
+++ generate_symlinks.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-ln -s ./.bash_profile .bash_profile
-ln -s ./.bash_theme ~/.bash_theme
-ln -s ./.bashrc ~/.bashrc
-ln -s ./.load-castlemacs.sh ~/.load-castlemacs.sh
-ln -s ./.unload-castlemacs.sh ~/.unload-castlemacs.sh
+ln -s ~/code/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/code/dotfiles/.bash_theme ~/.bash_theme
+ln -s ~/code/dotfiles/.bashrc ~/.bashrc
+ln -s ~/code/dotfiles/.load-castlemacs.sh ~/.load-castlemacs.sh
+ln -s ~/code/dotfiles/.unload-castlemacs.sh ~/.unload-castlemacs.sh
 
-# this path will change in Catalina
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/ ~/icloud
 
 ln -s ~/icloud/.os/.ssh .ssh
